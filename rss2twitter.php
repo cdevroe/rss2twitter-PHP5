2021606f425a14270fd7a8e28bb39eceaeb9604a
Colin Devroe <colin@cdevroe.com>
2010-12-29 12:22:59 -0500

    Now supports OAuth! Removed option to tweet only single category. Added Twitter class by Tijs Verkoyen. Improved caching. See Readme.textile for more.

--- rss2twitter.php
+++ rss2twitter.php
@@ -1,22 +1,41 @@
 <?php
+require_once('twitter.php');
 /* 
 	RSS to Twitter using PHP 5.
 	By: Colin Devroe
 	http://cdevroe.com/
-	
 	http://github.com/cdevroe
+	https://github.com/cdevroe/rss2twitter-PHP5
 	
 	Originally written on December 6, 2009 while watching
 	Star Trek III: The Search for Spock.
 	
-	Version 0.4 - January 25, 2010
+	Version 2.0 - December 29, 2010
+  See readme.textile for installation instructions, licensing, version history, etc.
 
 */
 
-$feedUrl = 'URL'; // e.g. http://cdevroe.com/feed/
-$categoryToTwitter = ''; // e.g. 'Mobile photos' (see Readme for more)
-$cacheDir = "/path/to/cachedir/"; // e.g. /home/.eastwood/domain.com/directory/'
-$twitter = array('username' => 'USERNAME', 'password' => 'PASSWORD');
+/* Setup */
+
+/* Initiate Twitter class 
+consumerKey, consumerSecret */
+$rss2twitter = new Twitter('','');
+
+// Setup OAuthToken,OAuthTokenSecret
+$rss2twitter->setOAuthToken('');
+$rss2twitter->setOAuthTokenSecret('');
+
+// Feed URL and Cache directory
+$feedUrl = 'http://cdevroe.com/feed/'; // e.g. http://cdevroe.com/feed/
+$cacheDir = "cache/"; // e.g. /home/.eastwood/domain.com/directory/' Leave blank to turn off caching
+
+// Error reporting level
+error_reporting(E_NOTICE); // You may change this for debugging purposes if you'd like.
+
+/* End Setup */
+
+
+
 
 // Retrieve and parse RSS feed
 $curl = curl_init($feedUrl);
@@ -29,9 +48,11 @@ $xml = str_replace("&#8217;","'",$xml); $xml = str_replace("&#160;","",$xml); //
 $parsedXml = new SimpleXMLElement($xml);
 
 // Retrieve and load Cache
-$cacheFile = $cacheDir . "twitterCache.txt"; $timeout = 12; // In hrs.
-if ($cached = @file_get_contents($cacheFile)) { $cached = unserialize($cached); }
-if (!$cached || !is_array($cached)) { $cached = array_fill(0, 19, "-"); }
+if ($cacheDir != '') {
+  $cacheFile = $cacheDir . "twitterCache.txt"; $timeout = 12; // In hrs.
+  if ($cached = @file_get_contents($cacheFile)) { $cached = unserialize($cached); }
+  if (!$cached || !is_array($cached)) { $cached = array_fill(0, 19, "-"); }
+}
 
 // Loop through RSS feed items and post to Twitter
 foreach ($parsedXml->channel->item as $post) {
@@ -44,34 +65,27 @@ foreach ($parsedXml->channel->item as $post) {
 		b. The post is less than (timeout) hours old.
 		c. The category is (categoryToTwitter) (optional) */
 	if (in_array($post->link, $cached) === false && ($date == NULL || $date > time() - (60 * 60 * $timeout))) {
-	
-		// If a category is provided, it will check for it.
-		if ($categoryToTwitter == '' || $post->category[0] == $categoryToTwitter) {
 			
-			// Construct message
-			$twitterMessage = $post->category[0].' "'.$post->title.'": '.$post->link;
-			
-			// Send tweet to Twitter
-			$curl = curl_init("http://twitter.com/statuses/update.xml?status=". urlencode($twitterMessage));
-				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-				curl_setopt($curl, CURLOPT_POST, true);
-				curl_setopt($curl, CURLOPT_USERPWD, $twitter['username'].":". $twitter['password']);
-				$tweet = curl_exec($curl);
-				$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-			curl_close($curl);
-			
-			// If success write to cache, else fail
-			if ($status == 200) { 
-				$cached[] = $post->link;
-				if (count($cached) > 50) { array_shift($cached); }
-			} else {
-				echo '<p>Tried to Twitter: '.$twitterMessage.' - Error status: '.$status;
-			} // End if status
-		} // End if categoryToTwitter
+		// Construct message
+		$tweet = $post->category[0].' "'.$post->title.'": '.$post->link;
+		
+		// Send tweet to Twitter
+		$sendTweet = $rss2twitter->statusesUpdate($tweet);
+		
+		print_r($sendTweet);
+		
+		// If success write to cache, else fail
+		if (isset($sendTweet['id'])) { 
+			$cached[] = $post->link;
+			if (count($cached) > 50) { array_shift($cached); }
+		} else {
+			echo '<p>There was an error.</p>';
+		} // End if status */
 	} // end if (post to twitter)
 } // end foreach
 
 // Open, Write, Close, cache.
-$f = fopen($cacheFile,"w+"); fwrite($f,serialize($cached)); fclose($f);
+if ($cacheDir != '') {
+  $f = fopen($cacheFile,"w+"); fwrite($f,serialize($cached)); fclose($f);
+}
 ?>
